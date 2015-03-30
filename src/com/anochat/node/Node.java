97e104ca2b59e2079982c954a85def21064db411
Shane Price <stp041@gmail.com>
2015-03-30 14:19:08 -0600

    What. Eclipse cannot generate a new project based off directory layout?

--- src/com/anochat/node/Node.java
+++
@@ -1,9 +0,0 @@
-package com.anochat.node;
-
-import java.net.Socket;
-import java.io.IOException;
-
-public interface Node {
-	public void addConnection(Socket socket) throws IOException;
-	public void onEvent(byte[] data) throws IOException;
-}
