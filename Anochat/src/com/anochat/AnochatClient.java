9ef8b56886d2104d0cea08079fe8ce56999bf3e7
Shane Price <stp041@gmail.com>
2015-09-26 12:36:46 -0600

    Trying something with moving the client to a separate thread

--- Anochat/src/com/anochat/AnochatClient.java
+++ Anochat/src/com/anochat/AnochatClient.java
@@ -12,12 +12,14 @@ import com.anochat.wireformats.Event;
 import com.anochat.wireformats.NodeSendsMessage;
 import com.anochat.wireformats.Protocol;
 
-public class AnochatClient implements Node {
+public class AnochatClient implements Node, Runnable {
 	
+	private final Object lock;
 	private final ThreadPoolManager threadPool;
 	private TCPConnection server;
 	
 	public AnochatClient() {
+		lock = new Object();
 		threadPool = new ThreadPoolManager(8);
 	}
 	
@@ -30,6 +32,7 @@ public class AnochatClient implements Node {
 		// unused
 	}
 	
+	@Override
 	public void onDisconnect(String id) throws IOException {
 		System.out.println("Disconnected from server!");
 	}
@@ -53,26 +56,52 @@ public class AnochatClient implements Node {
 	public void sendMessage(String message) throws IOException {
 		server.send(new NodeSendsMessage(message).getBytes());
 	}
-	
+
+	@Override
+	public void run() {
+		try {
+			connectToServer("localhost", 10865);
+			synchronized(lock) {
+				lock.wait();
+			}
+		} catch(Exception e) {
+		} 
+	}
+
+	public void stop() throws InterruptedException {
+		synchronized(lock) {
+			lock.notify();
+		}
+		System.out.println("Stopped!");
+	}
+
 	
 	public static void main(String[] args) {
-		try {
+		//try {
+			/*
 			if(args.length < 2) {
 				System.out.println("Please provide server and port");
 				System.exit(1);
 			}
-			String server = args[0];
-			int port = Integer.parseInt(args[1]);
+			*/
+			//String server = args[0];
+			//int port = Integer.parseInt(args[1]);
 			AnochatClient client = new AnochatClient();
-			client.connectToServer(server, port);
+			new Thread(client).start();
+			
+			//client.connectToServer(server, port);
+			/*
 			Scanner keyboard = new Scanner(System.in);
 			String message = keyboard.nextLine();
 			while(message != null && !message.equalsIgnoreCase("quit")) {
 				client.sendMessage(message);
 				message = keyboard.nextLine();
 			}
+			*/
+		/*
 		} catch(IOException ioe) {
 			System.err.println(ioe.getMessage());
 		}
+		*/
 	}
 }
